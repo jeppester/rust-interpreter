fc107d82f0e728d0d6e20b9315ead25d9b5ed2f4
Jesper Sørensen <jeppester1@gmail.com>
2020-09-18 15:06:58 +0200

    Object: Add functions for treating objects as booleans or numerics
    And fail gracefully if not possible

--- src/object/mod.rs
+++ src/object/mod.rs
@@ -1,4 +1,5 @@
 use std::fmt;
+use crate::eval::eval_error::*;
 
 #[derive(Debug)]
 pub enum ObjectType {
@@ -17,9 +18,20 @@ impl ObjectType {
   }
 }
 
+const TRUE: bool = true;
+const FALSE: bool = false;
+
 pub trait Object: fmt::Debug {
   fn get_type(&self) -> ObjectType;
   fn inspect(&self) -> String;
+
+  fn get_boolean_value(&self) -> Result<&bool, EvalError> {
+    Err(EvalError(format!("Expected boolean, found: {:?}", self.get_type())))
+  }
+
+  fn get_numeric_value(&self) -> Result<&i64, EvalError> {
+    Err(EvalError(format!("Expected numeric, found: {:?}", self.get_type())))
+  }
 }
 
 // Integer
@@ -36,6 +48,17 @@ impl Object for Integer {
   fn inspect(&self) -> String {
     self.value.to_string()
   }
+
+  fn get_boolean_value(&self) -> Result<&bool, EvalError> {
+    match self.value {
+      0 => Ok(&FALSE),
+      _ => Ok(&TRUE),
+    }
+  }
+
+  fn get_numeric_value(&self) -> Result<&i64, EvalError> {
+    Ok(&self.value)
+  }
 }
 
 // Boolean
@@ -51,6 +74,10 @@ impl Object for Boolean {
 
   fn inspect(&self) -> String {
     if self.value { "true".to_string() } else { "false".to_string() }
+  }
+
+  fn get_boolean_value(&self) -> Result<&bool, EvalError> {
+    Ok(&self.value)
   }
 }
 
