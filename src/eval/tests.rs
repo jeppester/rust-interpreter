ceaa981d3ed5b5a14f7fd69ee19f40348923a5bf
Jesper Sørensen <jeppester1@gmail.com>
2020-09-18 20:35:48 +0200

    Simplify tests with get_numeric_value and get_boolean_value

--- src/eval/tests.rs
+++ src/eval/tests.rs
@@ -6,8 +6,8 @@ use crate::object::*;
 #[test]
 fn test_eval_integer_expression() -> Result<(), String> {
   let tests = vec![
-    ("5", "5"),
-    ("10", "10"),
+    ("5", 5),
+    ("10", 10),
   ];
 
   for test in &tests {
@@ -22,8 +22,8 @@ fn test_eval_integer_expression() -> Result<(), String> {
 #[test]
 fn test_eval_boolean_expression() -> Result<(), String> {
   let tests = vec![
-    ("true", "true"),
-    ("false", "false"),
+    ("true", true),
+    ("false", false),
   ];
 
   for test in &tests {
@@ -38,14 +38,14 @@ fn test_eval_boolean_expression() -> Result<(), String> {
 #[test]
 fn test_eval_bang_operator() -> Result<(), String> {
   let tests = vec![
-    ("!true", "false"),
-    ("!false", "true"),
-    ("!5", "false"),
-    ("!0", "true"),
-    ("!!true", "true"),
-    ("!!false", "false"),
-    ("!!5", "true"),
-    ("!!0", "false"),
+    ("!true", false),
+    ("!false", true),
+    ("!5", false),
+    ("!0", true),
+    ("!!true", true),
+    ("!!false", false),
+    ("!!5", true),
+    ("!!0", false),
   ];
 
   for test in &tests {
@@ -66,12 +66,14 @@ fn test_eval(input: &str) -> Box<dyn Object> {
   match_or_fail!(eval(&program), Ok(m) => m)
 }
 
-fn test_integer_object(object: &Box<dyn Object>, result: &str) {
+fn test_integer_object(object: &Box<dyn Object>, result: &i64) {
   match_or_fail!(object.get_type(), ObjectType::Integer => ());
-  assert_eq!(object.inspect(), result);
+  let numeric_value = match_or_fail!(object.get_numeric_value(), Ok(m) => m);
+  assert_eq!(numeric_value, result);
 }
 
-fn test_boolean_object(object: &Box<dyn Object>, result: &str) {
+fn test_boolean_object(object: &Box<dyn Object>, result: &bool) {
   match_or_fail!(object.get_type(), ObjectType::Boolean => ());
-  assert_eq!(object.inspect(), result);
+  let boolean_value = match_or_fail!(object.get_boolean_value(), Ok(m) => m);
+  assert_eq!(boolean_value, result);
 }
